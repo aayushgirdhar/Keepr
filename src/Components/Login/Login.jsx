a0ba4bc7cd7590dc284e6f424cff52455341aed9
AayushG02 <aayush.girdhar2@gmail.com>
2024-04-05 23:42:11 +0530

    Add user menu and logout functionality

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,55 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../../firebase";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 
+import GoogleIcon from "@mui/icons-material/Google";
+import "./Login.css";
+
+const UserMenu = () => {
+  const { user, dispatch } = useContext(UserContext);
+  const [isHover, setHover] = useState(false);
+  return (
+    <div className="user-menu-container">
+      <img
+        src={user.img}
+        alt="profile"
+        className="user-img"
+        onClick={() => setHover((prev) => !prev)}
+      />
+      {isHover && (
+        <div
+          className="user-menu"
+          onMouseLeave={() =>
+            setTimeout(() => {
+              setHover(false);
+            }, 500)
+          }
+        >
+          <p>{user.name}</p>
+          <button
+            onClick={() => {
+              dispatch({
+                type: "SET_USER",
+                payload: {
+                  uid: "",
+                  name: "",
+                  email: "",
+                  img: "",
+                  notes: [],
+                },
+              });
+            }}
+            className="user-logout"
+          >
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 const Login = () => {
   const { user: localUser, dispatch } = useContext(UserContext);
   const handleLogin = async () => {
@@ -18,7 +64,6 @@ const Login = () => {
       const userDoc = await getDoc(userRef);
 
       if (!userDoc.exists()) {
-        console.log("user is not in the DB");
         const userData = {
           id: user.uid,
           name: user.displayName,
@@ -29,15 +74,24 @@ const Login = () => {
         await setDoc(userRef, userData);
         dispatch({ type: "SET_USER", payload: userData });
       } else {
-        console.log("user is in the db");
-        dispatch({ type: "SET_USER", payload: userDoc.data() });
+        const mergedNotes = [...localUser.notes, ...userDoc.data().notes];
+        await setDoc(userRef, { notes: mergedNotes }, { merge: true });
+        dispatch({
+          type: "SET_USER",
+          payload: { ...userDoc.data(), notes: mergedNotes },
+        });
       }
     } catch (error) {
       console.log(error);
     }
   };
 
-  return <button onClick={handleLogin}>Login with Google</button>;
+  return localUser.email !== "" ? (
+    <UserMenu />
+  ) : (
+    <button onClick={handleLogin} className="user-login">
+      <GoogleIcon /> Login
+    </button>
+  );
 };
-
 export default Login;
