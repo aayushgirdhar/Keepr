76666883e2e5184d7a928e860315bccea9b23e73
AayushG02 <aayush.girdhar2@gmail.com>
2024-04-05 23:42:28 +0530

    Add edit functionality to Note component

--- src/Components/Note/Note.jsx
+++ src/Components/Note/Note.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { useContext } from "react";
@@ -6,11 +7,16 @@ import { UserContext } from "../../context/UserContext";
 import { formatDistanceToNow } from "date-fns";
 
 import "./Note.css";
+import DoneIcon from "@mui/icons-material/Done";
+
 import { arrayRemove, doc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 
 const Note = ({ note }) => {
   const { user, dispatch } = useContext(UserContext);
+  const [isEdit, setEdit] = useState(false);
+  const [title, setTitle] = useState(note.title);
+  const [text, setText] = useState(note.text);
 
   const handleDelete = async (note) => {
     dispatch({ type: "DELETE_NOTE", payload: note.id });
@@ -23,11 +29,46 @@ const Note = ({ note }) => {
     }
   };
 
+  const handleChange = async () => {
+    const updatedNotes = user.notes.map((n) => {
+      if (n.id === note.id) {
+        return { ...note, title, text, isEdit: true };
+      } else {
+        return n;
+      }
+    });
+    console.log(updatedNotes);
+    dispatch({ type: "EDIT_NOTE", payload: updatedNotes });
+    const userRef = doc(db, "users", user.id);
+    await updateDoc(userRef, {
+      notes: updatedNotes,
+    });
+    setEdit(false);
+  };
+
   return (
     <div className="note">
       <div className="note-body">
-        <h2 className="note-title">{note.title}</h2>
-        <p className="note-content">{note.text}</p>
+        <h2
+          contentEditable={isEdit}
+          suppressContentEditableWarning={true}
+          onInput={(e) => {
+            setTitle(e.currentTarget.textContent);
+          }}
+          className="note-title"
+        >
+          {note.title}
+        </h2>
+        <p
+          className="note-content"
+          contentEditable={isEdit}
+          suppressContentEditableWarning={true}
+          onInput={(e) => {
+            setText(e.currentTarget.textContent);
+          }}
+        >
+          {note.text}
+        </p>
       </div>
       <div className="note-footer">
         <div className="left">
@@ -39,9 +80,16 @@ const Note = ({ note }) => {
           </div>
         </div>
         <div className="right">
-          <button className="note-btn">
-            <EditIcon />
-          </button>
+          {isEdit ? (
+            <button className="note-btn" onClick={handleChange}>
+              <DoneIcon />
+            </button>
+          ) : (
+            <button className="note-btn" onClick={() => setEdit(true)}>
+              <EditIcon />
+            </button>
+          )}
+
           <button className="note-btn" onClick={() => handleDelete(note)}>
             <DeleteIcon />
           </button>
